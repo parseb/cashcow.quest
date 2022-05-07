f6ad441b2757b75713e8c7587d2c7d108a90c2d7
parseb <petra306@protonmail.com>
2022-05-07 23:48:02 +0300

    test 0

--- tests/0_default_test.py
+++ tests/0_default_test.py
@@ -1,13 +1,14 @@
 import pytest
 from brownie import accounts, chain, ZERO_ADDRESS, interface
-from tests.conftest import CashCow, DAI, ValueConduct, IV3Factory, isPolygon
+from tests.conftest import CCOW, DAI, VC, IV3Factory, isPolygon
 
 
 
-def test_default(isPolygon, CashCow, ValueConduct, DAI, IV3Factory):
+def test_default(isPolygon, CCOW, VC, DAI, IV3Factory):
     assert isPolygon
-    assert CashCow.address != ZERO_ADDRESS
-    
+    assert CCOW.address != ZERO_ADDRESS
+    assert interface.IERC20(VC.address).balanceOf(accounts[-2]) > 100_000
+    assert interface.IERC20(DAI.address).balanceOf(accounts[-1]) > 100_000
 
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,29 +8,28 @@ V3Factory = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
 
 @pytest.fixture
 def isPolygon():
-    return chain._chainid == 137
+    return chain.id == 137
 
 @pytest.fixture
-def CashCow():
-    deployed1 = CashCow.deploy({'from': accounts[0]})
+def CCOW():
+    deployed1 = CashCow.deploy(DAI_ADDR, V3Factory, {'from': accounts[0]})
     return deployed1
 
 @pytest.fixture
-def ValueConduct():
+def VC():
     deployed2 = ValueConduct.deploy('0xe7b30a037f5598e4e73702ca66a59af5cc650dcd',{'from': accounts[0]})
     print("ValueConduct deplyed at: ", deployed2.address)
     return deployed2
 
 @pytest.fixture
 def DAI():
-    DAI = interface.IERC20(DAI_ADDR)
-    print("DAI wrapped. balance of accounts[0] - address: ", DAI.balanceOf(accounts[0]), DAI.address)
-    return DAI
+    D = interface.IERC20(DAI_ADDR)
+    print("DAI wrapped. balance of accounts[0] - address: ", D.balanceOf(accounts[0]), D.address)
+    return D
 
 @pytest.fixture
 def IV3Factory():
     factory = interface.IUniswapV3Factory(V3Factory)
-    print("DAI wrapped. balance of accounts[0] - address: ", DAI.balanceOf(accounts[0]), DAI.address)
     return factory
 
 
