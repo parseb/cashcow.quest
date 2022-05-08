1166800f8d42f1be08a4a6b91bc8a1037b9334af
parseb <petra306@protonmail.com>
2022-05-08 17:44:23 +0300

    deal proposed

--- contracts/CashCow.sol
+++ contracts/CashCow.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity  0.8.13;
+pragma solidity 0.8.13;
 
 /// @title CashCow.quest main
 /// @author parseb.eth
@@ -7,27 +7,106 @@ pragma solidity  0.8.13;
 /// @dev Experimental. Do not use.
 /// @custom:security contact: petra306@protonmail.com
 
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '../interfaces/IUniswapV3Pool.sol';
-import '../interfaces/IUniswapV3Factory.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../interfaces/IUniswapV3Pool.sol";
+import "../interfaces/IUniswapV3Factory.sol";
 
-contract CashCow is ERC721('Cash Cow Quest', 'COWQ') {
-    address immutable zero = 0x0000000000000000000000000000000000000000;
+contract CashCow is ERC721("Cash Cow Quest", "COWQ") {
     uint256 immutable MAXUINT = type(uint256).max;
     address immutable UNISWAPV3_ADDRESS;
     address immutable DAIaddr;
 
+    uint256 public tempId;
+
+    //// Errors
+    error TokenTransferFailed(address _token, uint256 _amount);
+
+    //// Events
+    event NewDealProposed(address indexed _token, uint256 indexed _Tempid);
+    event RefundedNoDeal(uint256 indexed _Tempid, address indexed _token, address _caller);
 
     constructor(address _dai, address _uniV3factory) {
         UNISWAPV3_ADDRESS = _uniV3factory;
         DAIaddr = _dai;
-    } 
+        tempId = 1;
+    }
+
+    struct Cow {
+        address[3] owners; //[seller, buyer, projectToken]
+        uint256[2] amounts; //[giveAmount, takeAmount]
+        uint256[2] vestStartEnd; //[vestStart, vestEnd]
+        string data; //url
+    }
+
+    /// @notice Stores Cow with getter function from 721-721 ID
+    mapping(uint256 => Cow) cashCowById;
+
+    /// @notice Proposes a new deal
+    /// @param _projectToken address of offered token
+    /// @param _giveAmountx100 amount of offered tokens
+    /// @param _wantsAmountx100 amount of DAI per _projectToken (1=0.01) [1 _projectToken <-> _wantsAmount * 100]
+    /// @param _vestStart vesting period starts _ days after deal minted
+    /// @param _vestEnd vesting period ends _ days after _vestStart
+    function createDeal(
+        address _projectToken,
+        uint256 _giveAmountx100,
+        uint256 _wantsAmountx100,
+        uint256 _vestStart,
+        uint256 _vestEnd,
+        string memory _pitchDataURL
+    ) public returns (uint256 tId) {
+        require(_projectToken != address(0), "Token is zero");
+        require(bytes(_pitchDataURL).length <= 32, "URL too long");
+        require(
+            _wantsAmountx100 * _giveAmountx100 * _vestStart * _vestEnd > 0,
+            "0 value not allowed"
+        );
+
+        if (
+            IERC20(_projectToken).transferFrom(
+                msg.sender,
+                address(this),
+                _giveAmountx100 * 10**16
+            )
+        ) {
+            cashCowById[tempId] = Cow(
+                [msg.sender, address(0), _projectToken],
+                [_giveAmountx100 * 10 ** 16, _wantsAmountx100 * 10**16],
+                [_vestStart, _vestEnd],
+                _pitchDataURL
+            );
+
+            tId = tempId;
+            unchecked {
+                tempId++;
+            }
+
+            emit NewDealProposed(_projectToken, tId);
+        } else {
+            revert TokenTransferFailed(_projectToken, _giveAmountx100 * 10**16);
+        }
+    }
 
 
-    
+    function reclaimNone(uint256 _id) public returns (bool s) {
 
+        Cow memory cow = cashCowById[_id];
+        require(cow.owners[0] == msg.sender && cow.owners[1] == address(0), "Cow Unreachable");
+        delete cashCowById[_id];
 
+        s = IERC20(cow.owners[2]).transfer(msg.sender, cow.amounts[0]);
+        require(s, "Refund Failed");
 
+        emit RefundedNoDeal(_id, cow.owners[2], msg.sender);
 
+    }
+    // _setTokenURI(_tempId, "https://cashcow.quest/token/" + _tempId);
+
+    /// VIEW FUNCTIONS
+
+    function getCashCowById(uint256 _id) public view returns (Cow memory) {
+        return cashCowById[_id];
+    }
 }
--- tests/0_default_test.py
+++ tests/0_default_test.py
@@ -10,14 +10,26 @@ def test_default(isPolygon, CCOW, VC, DAI, IV3Factory):
     assert interface.IERC20(VC.address).balanceOf(accounts[-2]) > 100_000
     assert interface.IERC20(DAI.address).balanceOf(accounts[-1]) > 100_000
     assert IV3Factory.owner() != ZERO_ADDRESS
+    # assert DAI.decimals() == 18
+    assert VC.decimals() == 18
 
 def test_creates_deal(CCOW, DAI, VC):
-    dealId = CCOW.createDeal(VC.address, 1_000, 500, 100_000, 356, 356, {'from': accounts[0]})
+    tempId = CCOW.tempId({'from': accounts[0]})
+        # address _projectToken,
+        # uint256 _giveAmount,
+        # uint256 _wantsAmount,
+        # uint256 _vestStart,
+        # uint256 _vestEnd,
+        # string memory _pitchDataURL
+    DAI.transfer(accounts[1].address, 500000*10**18,  {"from": accounts[-1]})
+    VC.approve(CCOW.address, 100000*10**18, {"from": accounts[0]})
+    dealId = CCOW.createDeal(VC.address, 1_000, 20, 356, 356, "12345678912345678912345678900012", {'from': accounts[0]}).return_value
+
     assert dealId > 0
-    deal = CCOW.getDeal(dealId)
+    deal = CCOW.getCashCowById(dealId)
     assert deal[0][0] == accounts[0] #deal creator
     assert deal[0][1] == ZERO_ADDRESS #deal taker - deal open
-    assert deal[1][0] == 1_000 #project token amount
-    assert deal[1][1] == 500 #project token amount
-    assert deal[2][0] > chain.time #deal start time
-    assert deal[2][1] > deal[2][0] * 8640 + chain.time  #deal end time
+    assert deal[0][2] == VC.address #proposed token
+    assert deal[1][0] == 1_000 * 10 ** 16 #project token amount
+    assert deal[1][1] == 20 * 10 ** 16 #project token amount
+    assert tempId < CCOW.tempId({'from': accounts[0]})
